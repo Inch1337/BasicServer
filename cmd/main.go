6fabb4c526475b8d26d69a8e6892aa530cf5607d
Inch1337 <merkulovd2005@gmail.com>
2026-01-19 03:19:29 +0400

    removed Handlers from main

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"product-api/internal/config"
 	"product-api/internal/database"
-	"strconv"
+	"product-api/internal/handlers"
 
 	_ "github.com/lib/pq"
 )
-
-type Product struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
-}
-
-var (
-	products  = []Product{}
-	idCounter = 1
-)
-
-var DB *sql.DB
 
 func main() {
 	config.Load()
@@ -35,87 +19,14 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", homeHandler)
-	mux.HandleFunc("/health", healthHandler)
-	mux.HandleFunc("/products", ProductsHandler)
-	mux.HandleFunc("/products/", GetProductHandler)
+	mux.HandleFunc("/", handlers.HomeHandler)
+	mux.HandleFunc("/health", handlers.HealthHandler)
+	mux.HandleFunc("/products", handlers.CreateProduct)
+	mux.HandleFunc("/products/", handlers.GetProduct)
 
 	fmt.Printf("Start server on %s\n", cfg.ServerPort)
 
 	if err := http.ListenAndServe(cfg.ServerPort, mux); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	fmt.Fprint(w, "Welcome to Product API")
-}
-
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	fmt.Fprint(w, "Service running")
-}
-
-func ProductsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(products)
-
-	case "POST":
-		var p Product
-
-		err := json.NewDecoder(r.Body).Decode(&p)
-		if err != nil {
-			http.Error(w, "Bad JSON", http.StatusBadRequest)
-			return
-		}
-
-		p.ID = idCounter
-		idCounter++
-
-		products = append(products, p)
-
-		w.Header().Set("Content-Type", "application/json")
-
-		json.NewEncoder(w).Encode(p)
-
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func GetProductHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	idStr := r.URL.Path[len("/products/"):]
-
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-
-	for _, item := range products {
-		if item.ID == id {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(item)
-
-			return
-		}
-	}
-
-	http.Error(w, "Product not found", http.StatusNotFound)
 }
---
+++ internal/handlers/common.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprint(w, "Welcome to Product API")
+}
+
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Service is running"))
+}
